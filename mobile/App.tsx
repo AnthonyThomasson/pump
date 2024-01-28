90d4b00bd0e914d21a5717140d3b6367322af4d2
Anthony Thomasson <athomasson.93@gmail.com>
2024-01-28 04:55:32 +0000

    Add Gluestick

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,63 @@
-import '@expo/metro-runtime';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ButtonText } from '@gluestack-ui/themed';
 
+import { config } from '@gluestack-ui/config'; // Optional if you want to use default theme
+import {
+  Box,
+  Button,
+  GluestackUIProvider,
+  Input,
+  InputField,
+  InputIcon,
+  InputSlot,
+  SearchIcon,
+  Text,
+} from '@gluestack-ui/themed';
+import React from 'react';
 export default function App() {
+  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState('');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <Box width="100%" justifyContent="center" alignItems="center">
+        <Text>Open up App.js to start working on your app!</Text>
+
+        <Input size={'xl'} variant={'rounded'} isInvalid={false} isDisabled={false}>
+          <InputField
+            onChange={(e: any) => {
+              setEmail(e.nativeEvent.text);
+            }}
+            value={email}
+            placeholder="Email"
+          />
+          <InputSlot pr="$4">
+            <InputIcon as={SearchIcon} />
+          </InputSlot>
+        </Input>
+
+        <Input size={'xl'} variant={'rounded'} isInvalid={false} isDisabled={false}>
+          <InputField
+            onChange={(e: any) => {
+              setPassword(e.nativeEvent.text);
+            }}
+            value={password}
+            type="password"
+            placeholder="Password"
+          />
+          <InputSlot pr="$4">
+            <InputIcon as={SearchIcon} />
+          </InputSlot>
+        </Input>
+
+        <Button
+          onPress={() => console.log('I will login')}
+          action={'primary'}
+          variant={'solid'}
+          size={'lg'}
+          isDisabled={false}
+        >
+          <ButtonText>Login</ButtonText>
+        </Button>
+      </Box>
+    </GluestackUIProvider>
   );
 }
-const backgroundColor = '#fff';
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: backgroundColor,
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
