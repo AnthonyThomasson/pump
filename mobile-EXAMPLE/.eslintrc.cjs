91e2f2779f77fb9e7b58c90e1ca5f61e6e1a2b38
Anthony Thomasson <athomasson.93@gmail.com>
2024-01-28 00:23:00 +0000

    Update example App

---
+++ mobile-EXAMPLE/.eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+module.exports = {
+  plugins: ['@typescript-eslint', 'react', 'react-native', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-native/all',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  root: true,
+}
--- mobile-EXAMPLE/components/EditScreenInfo.tsx
+++ mobile-EXAMPLE/components/EditScreenInfo.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React from 'react'
+import { StyleSheet } from 'react-native'
 
-import { ExternalLink } from './ExternalLink';
-import { MonoText } from './StyledText';
-import { Text, View } from './Themed';
+import { ExternalLink } from './ExternalLink'
+import { MonoText } from './StyledText'
+import { Text, View } from './Themed'
 
-import Colors from '@/constants/Colors';
+import Colors from '@/constants/Colors'
 
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
@@ -14,49 +14,52 @@ export default function EditScreenInfo({ path }: { path: string }) {
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
+          darkColor="rgba(255,255,255,0.8)"
+        >
           Open up the code for this screen:
         </Text>
 
         <View
           style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
           darkColor="rgba(255,255,255,0.05)"
-          lightColor="rgba(0,0,0,0.05)">
+          lightColor="rgba(0,0,0,0.05)"
+        >
           <MonoText>{path}</MonoText>
         </View>
 
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
+          darkColor="rgba(255,255,255,0.8)"
+        >
+          Change any of the text, save the file, and your app will automatically
+          update.
         </Text>
       </View>
 
       <View style={styles.helpContainer}>
         <ExternalLink
           style={styles.helpLink}
-          href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet">
+          href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet"
+        >
           <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
+            Tap here if your app doesn't automatically update after making
+            changes
           </Text>
         </ExternalLink>
       </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
   codeHighlightContainer: {
     borderRadius: 3,
     paddingHorizontal: 4,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
   },
   getStartedText: {
     fontSize: 17,
@@ -64,9 +67,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   helpContainer: {
-    marginTop: 15,
-    marginHorizontal: 20,
     alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 15,
   },
   helpLink: {
     paddingVertical: 15,
@@ -74,4 +77,7 @@ const styles = StyleSheet.create({
   helpLinkText: {
     textAlign: 'center',
   },
-});
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+})
