c3fe4f98b82d92618d3d40d203d9e2abe137e563
Anthony Thomasson <athomasson.93@gmail.com>
2024-02-04 21:08:13 +0000

    Adding in router

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,26 +1,21 @@
 import '@expo/metro-runtime';
 
 import { User } from '@/hooks/useAuth';
-import LandingPage from '@/pages/LandingPage';
 import LoginPage from '@/pages/LoginPage';
 import { config } from '@gluestack-ui/config';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { useState } from 'react';
-export default function App() {
+export default function Page() {
   const [token, setToken] = useState<string | null>(null);
   const [user, setUser] = useState<User | null>(null);
   return (
     <GluestackUIProvider config={config} colorMode="dark">
-      {token && user ? (
-        <LandingPage token={token} user={user} />
-      ) : (
-        <LoginPage
-          onLogin={(token: string, user: User) => {
-            setToken(token);
-            setUser(user);
-          }}
-        />
-      )}
+      <LoginPage
+        onLogin={(token: string, user: User) => {
+          setToken(token);
+          setUser(user);
+        }}
+      />
     </GluestackUIProvider>
   );
 }
